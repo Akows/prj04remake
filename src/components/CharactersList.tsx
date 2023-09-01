156aee8fa25bf00893b4898b6bc623464ed3a26e
AKOWSPC <akows141@gmail.com>
2023-09-01 20:37:12 +0900

    Feat: 캐릭터 목록 /  무기 목록 컴포넌트 구현 및 메인 페이지에 연결.

---
+++ src/components/CharactersList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CharactersList: React.FC = () => {
+  return <div>CharactersList</div>;
+};
+
+export default CharactersList;
---
+++ src/components/WeaponsList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WeaponsList: React.FC = () => {
+  return <div>WeaponsList</div>;
+};
+
+export default WeaponsList;
