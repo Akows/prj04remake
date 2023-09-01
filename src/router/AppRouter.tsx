3ba02eca3b76b297f51019ae50609e0912874536
AKOWSPC <akows141@gmail.com>
2023-09-01 20:21:26 +0900

    Feat: Router 컴포넌트에 메인 / 소개 페이지를 연결.

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import MainPage from '../pages/MainPage';
+import IntroducePage from '../pages/IntroducePage';
 
 const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={MainPage} />
+        <Route path="/introduce" Component={IntroducePage} />
       </Routes>
     </BrowserRouter>
   );
