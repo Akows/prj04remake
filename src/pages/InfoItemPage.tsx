78b07cd5ad3bc9eb3ceaaa1d23957e1f89fadbe6
AKOWSPC <akows141@gmail.com>
2023-09-02 23:35:44 +0900

    Fix: state 방식으로 데이터를 넘겨받을 경우 발생하는 딥 링크 문제를 해결. (state로 넘겨받은 데이터가 존재하지 않을 경우, 컴포넌트 렌더링을 중단하고 경고창 출력 후 메인 페이지로 리다이렉트 처리.)

--- src/pages/InfoItemPage.tsx
+++ src/pages/InfoItemPage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import {
   ItemOuterWrapper,
   ItemInfoWrapper,
@@ -10,14 +10,44 @@ import {
 } from '../styles/InfoListCommonStyles';
 
 const InfoItemPage: React.FC = () => {
-  const location = useLocation();
-  const characterData = location.state?.characterData;
+  // 상세 페이지에서는 데이터를 어떻게 다루어야 하는가?
+  // 제일 간단한 방법 : param으로 키워드만 받아서 데이터를 다시 호출.
+  // 혹은 이전 컴포넌트에서 useNavigate나 Link 태그로 state 전달.
+  // 다만 이 방법의 경우 상세 페이지 url을 복사 붙여넣기 할 경우 페이지가 렌더링 되지 않는다.
 
   const { name } = useParams();
 
+  const location = useLocation();
+  const characterData = location.state?.characterData;
+
+  const capitalizeFirstLetter = (str: string): string => {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  };
+
+  const getCharacterImagePath = (characterName: string) => {
+    const formattedName = capitalizeFirstLetter(characterName);
+    const encodedName = encodeURIComponent(formattedName); // 인코딩 추가
+    return `/assets/characterImage/${encodedName}.png`;
+  };
+
+  const imagePath = getCharacterImagePath(`${name}`);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!characterData) {
+      alert('잘못된 접근입니다.'); // 경고창 출력
+      navigate('/'); // 메인 페이지로 리다이렉트
+    }
+  }, [characterData, navigate]);
+
+  if (!characterData) {
+    return null;
+  }
+
   return (
     <>
-      <ItemOuterWrapper imagePath={`/assets/characterImage/${name}.png`}>
+      <ItemOuterWrapper imagePath={imagePath}>
         <ItemInfoWrapper>
           <ItemName>{characterData.name}</ItemName>
           <ItemDetails>
