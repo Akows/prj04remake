8b68855dad3100f52ba756605b6d16322b044bb5
AKOWSPC <akows141@gmail.com>
2023-09-02 17:33:22 +0900

    Design: 프로젝트의 배경 이미지 설정. (배경 이미지는 영역 전체에 꽉 차게 조절되며 중앙 정렬, fixed 속성을 걸어 스크롤을 내리거나 해도 이미지가 변하지 않도록 설정.)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import AppRouter from './router/AppRouter';
 import { AppDataProvider } from './context/AppDataContext';
+import backgroundImage from './assets/background.jpg';
 
 // React.FC?
 // Function Component의 약자로, 컴포넌트의 타입을 나타낸다.
@@ -24,4 +25,10 @@ const CenteredContainer = styled.div`
   display: flex; // flexbox를 사용
   justify-content: center; // 수평 중앙 정렬
   align-items: flex-start; // 수직 중앙 정렬
+
+  background-image: url(${backgroundImage}); // 여기에 원하는 배경 이미지 경로를 입력하세요
+  background-size: cover; // 배경 이미지가 컨테이너 크기에 꽉 차게 조절됩니다.
+  background-repeat: no-repeat; // 이미지가 반복되지 않게 합니다.
+  background-position: center; // 이미지의 중앙이 컨테이너의 중앙과 일치하게 합니다.
+  background-attachment: fixed; // 스크롤을 내려도 배경 이미지가 고정됩니다.
 `;
