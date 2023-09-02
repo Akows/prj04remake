262c11c38ce78be38bba3ce3a8dcb919ca9db8e3
AKOWSPC <akows141@gmail.com>
2023-09-02 23:06:39 +0900

    Fix: DB에서 데이터를 가지고 올 때, 무한 스크롤 사용 로직으로 인해 최초 데이터가 2번 호출되어 화면에 중복 렌더링 되는 문제를 해결. (데이터를 저장하는 방식을 객체로 변경, 페이지 번호를 key로, 데이터를 value로 설정하여 데이터를 저장. 1번 페이지의 데이터가 2번 저장되는 문제를 방지함.)

--- src/components/Info/ListContainer.tsx
+++ src/components/Info/ListContainer.tsx
@@ -10,14 +10,23 @@ const ListContainer: React.FC = () => {
   const { page, lastElementRef } = useInfiniteScroll();
   const { data, loading, error } = useFetchCharacters(page);
 
+  // 데이터 중복 렌더링 방지 로직.
+  // 커스텀 훅에서 저장하는 데이터는 이제 객체 형식이므로 map 함수로 렌더링 할 수가 없다.
+  // 따라서 객체 데이터를 배열 데이터로 변환해주어야 한다.
+  // Object.values()를 사용하게 되면 객체 데이터의 values 값만을 추출하여 배열 형식으로 반환한다.
+  // 이때 key 값이 숫자라면 숫자 오름차순으로 정렬되니 주의.
+  // 다음으로는 flat(). 배열로 전환했다고 해도 2차원 배열 등으로 배열 속의 배열이 존재하는 경우가 있다.
+  // 이때는 flat() 함수를 사용하여 1차원 배열로 바꾸어줘야 한다.
+  const allData = Object.values(data).flat();
+
   if (loading) return <Loading />;
   if (error) return <Error />;
 
   return (
     <ListContainers>
       <ListBody>
-        {data.map((item, index) => {
-          const isLastElement = data.length === index + 1;
+        {allData.map((item, index) => {
+          const isLastElement = allData.length === index + 1;
           return (
             <ListItem
               key={item.name}
--- src/hooks/useFetchCharacters.tsx
+++ src/hooks/useFetchCharacters.tsx
@@ -4,7 +4,9 @@ import axios from 'axios';
 const ITEMS_PER_PAGE = 10;
 
 const useFetchCharacters = (page: number) => {
-  const [data, setData] = useState<any[]>([]);
+  // Record?
+  // 객체의 키와 값을 타입으로 강제하는 데 사용.
+  const [data, setData] = useState<Record<number, any[]>>({});
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<null | string>(null);
 
@@ -14,12 +16,18 @@ const useFetchCharacters = (page: number) => {
         const response = await axios.get(`https://api.genshin.dev/characters`);
         const characterNames = response.data;
 
+        // 1 페이지에 해당되는 데이터만을 호출하기 위해 캐릭터 이름을 10 단위로 slice.
+        // 현재 페이지가 1이라면, (1 - 1) * 10 = 0이므로 시작 인덱스는 0.
+        // 현재 페이지가 2라면, (2 - 1) * 10 = 10이므로 시작 인덱스는 10.
         const slicedNames = characterNames.slice(
           (page - 1) * ITEMS_PER_PAGE,
           page * ITEMS_PER_PAGE,
         );
 
-        // 각 캐릭터의 데이터를 가져옵니다.
+        // 10개의 이름이 slice되었으니 이제 이를 바탕으로 데이터를 호출한다.
+        // 이 로직에서는 1 페이지당 호출을 10번씩 하고 있어서 너무 비효율적이지만..
+        // 이 API에서는 페이지네이션을 위한 param등을 지원하지 않고..
+        // 기본 주소에는 달랑 캐릭터 이름만을 반환하기 때문에 목록 컴포넌트에서 이름 이외에 부가적인 정보를 출력하기 위해서는 어쩔 수 없이 이런 방법을 선택해야한다.
         const characterDataPromises = slicedNames.map((name: string) =>
           axios.get(`https://api.genshin.dev/characters/${name}`),
         );
@@ -27,7 +35,10 @@ const useFetchCharacters = (page: number) => {
         const characterDataResponses = await Promise.all(characterDataPromises);
         const characterData = characterDataResponses.map(res => res.data);
 
-        setData(prevData => [...prevData, ...characterData]);
+        // 데이터의 저장 방식을 배열에서 객체로 변경.
+        // 페이지 번호를 key로, 데이터를 value로 지정하여 저장.
+        // 따라서 같은 페이지의 데이터는 덮어씌워지지, 2번 저장되지 않는다.
+        setData(prevData => ({ ...prevData, [page]: characterData }));
         setLoading(false);
       } catch (err) {
         setError('Error fetching data');
